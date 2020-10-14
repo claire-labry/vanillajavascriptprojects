db5db0b2cc9778b40bdb56a501052833b9b59365
claire-labry <clairestjlabry@gmail.com>
2020-10-14 10:41:30 -0500

    pausing-- left off at 06:11[

--- project5:domarraymethods/script.js
+++ project5:domarraymethods/script.js
@@ -30,4 +30,18 @@ async function getRandomUser() {
 
 function addData(obj){
     data.push(obj);
+
+    updateDOM();
+}
+
+// function to update DOM
+function updateDOM(providedData = data) {
+    // clear main div
+    main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+    providedData.forEach(item => {
+        const element = document.createElement('div');
+        element.classList.add('person');
+        element.innerHTML = `<strong>${item.name}</strong> ${item.money}`
+    })
 }
